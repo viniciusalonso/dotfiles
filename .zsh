122d37d2d86d3c49e34d12fd9b6ce8b9909d8673
Vinícius A <vba321@hotmail.com>
2018-04-10 23:31:38 -0300

    First commit

---
+++ .zsh
@@ -0,0 +1,24 @@
+ZSH_THEME="robbyrussell"
+
+plugins=(
+  git
+)
+
+source $ZSH/oh-my-zsh.sh
+
+# My personal configs
+
+export BROWSER=google-chrome
+export EDITOR=vim
+
+# Config
+alias zshconfig='$EDITOR ~/.zshrc && source ~/.zshrc'
+alias vimconfig='$EDITOR ~/.vimrc'
+
+# Online
+alias wiki='$BROWSER http://alonso.pbworks.com'
+alias whatsapp='$EDITOR https://web.whatsapp.com'
+alias skype='$EDITOR https://web.skype.com'
+
+setopt HIST_IGNORE_ALL_DUPS
+
