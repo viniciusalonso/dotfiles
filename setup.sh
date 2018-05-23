312c0b71545656a5024cef1fc9a7f945dc2062cd
Vinícius A <vba321@hotmail.com>
2018-05-23 06:20:30 -0300

    Setup vim config

--- setup.sh
+++ setup.sh
@@ -1,41 +1,31 @@
 #!/bin/bash
-export RED='\033[0;31m'
-export GREEN='\033[0;32m'
-export NC='\033[0m'
-
-#echo -e "${RED}Creating symbolik links${NC}"
-
-read -p "We're remove your old dotfiles, are you sure about go on? (Y/N)" -n 1 -r
+read -p "We'll remove your old vim files, are you sure about go on? (Y/N)" -n 1 -r
 printf "\n"
 
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  printf "Removing your dotfiles \n"
+  DOTFILES_PATH=$(pwd)
+
+  printf "Removing your vim files \n"
   rm $HOME/.vimrc 2> /dev/null
   rm -rf $HOME/.vim 2> /dev/null
-  #rm $HOME/.zshrc
   
-  printf "Creating symbolik links between dotfiles \n"
+  printf "Creating symbolik links between vim files \n"
   
-  ln -s $(pwd)/.vimrc $HOME/.vimrc
-  ln -s $(pwd)/.vim $HOME/.vim
-  #ln -s .zshrc $HOME/.zshrc
+  ln -s DOTFILES_PATH/.vimrc $HOME/.vimrc
+  ln -s DOTFILES_PATH/.vim $HOME/.vim
 
- printf "Instaling pluggins manager for vim \n"
+  printf "Instaling pluggins manager for vim \n"
 
   curl ~/.vim/autoload/plug.vim --create-dirs \
      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
  
   printf "Instaling dependencies \n"
-  P=$(pwd) 
-  sudo apt-get install -y build-essential cmake cmake3 python-dev
-  #sudo apt-get install python-dev #python-3-dev
+
+  sudo apt-get install -y build-essential cmake cmake3 python-dev 2> /dev/null
   cd ~/.vim/plugged/YouCompleteMe
   ./install.py
-  cd P
-
-  
+  cd DOTFILES_PATH
 
   printf "Instaling vim pluggins \n"
   vim -e +PlugInstall +q
